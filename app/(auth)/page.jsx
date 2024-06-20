e142b2c8643de7b9b50deaad50b2fddfd957028f
Blackstocks <ankitkumarsoni656.iitkha@gmail.com>
2024-06-20 14:52:41 +0530

    Update login component with logo1.svg and other adjustments

--- app/(auth)/page.jsx
+++ app/(auth)/page.jsx
@@ -13,32 +13,14 @@ const Login = () => {
       <div className='loginwrapper'>
         <div className='lg-inner-column'>
           <div className='left-column relative z-[1]'>
-            <div className='max-w-[520px] pt-20 ltr:pl-20 rtl:pr-20'>
-              <Link href='/'>
-                {/* <img
-                  src={
-                    isDark
-                      ? '/assets/images/logo/logo-white.svg'
-                      : '/assets/images/logo/logo.svg'
-                  }
-                  alt=''
-                  className='mb-10'
-                /> */}
-              </Link>
-              <h4>
-                Company |{' '}
-                <span className='text-slate-800 dark:text-slate-400 font-bold'>
-                  Logo
-                </span>
-              </h4>
-            </div>
-            {/* <div className='absolute left-0 2xl:bottom-[-160px] bottom-[-130px] h-full w-full z-[-1]'>
+            <div className='absolute left-0 2xl:bottom-[-10px] bottom-[-10px] h-full w-full z-[-1]'>
               <img
-                src='/assets/images/auth/ils1.svg'
+                src='/assets/images/auth/logo1.svg'
                 alt=''
                 className='h-full w-full object-contain'
+                
               />
-            </div> */}
+            </div>
           </div>
           <div className='right-column relative'>
             <div className='inner-content h-full flex flex-col bg-white dark:bg-slate-800'>
@@ -58,12 +40,8 @@ const Login = () => {
                 </div>
                 <div className='text-center 2xl:mb-10 mb-4'>
                   <h4 className='font-medium'>Sign in</h4>
-                  <div className='text-slate-500 text-base'>
-                    Sign in to Company Logo
-                  </div>
                 </div>
                 <LoginForm />
-
                 <div className='md:max-w-[345px] mx-auto font-normal text-slate-500 dark:text-slate-400 mt-12 uppercase text-sm'>
                   Don’t have an account?{' '}
                   <Link
